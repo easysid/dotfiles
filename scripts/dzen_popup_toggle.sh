568c40ccb502a838382e4836e6e4ce0a98f551c6
easysid <easysid+github@gmail.com>
2015-02-14 16:03:59 +0530

    better regex to filter popups

--- scripts/dzen_popup_toggle.sh
+++ scripts/dzen_popup_toggle.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-POPUP="popup_$1"
-COMMAND="dzen_$POPUP.sh"
+REGEX="dzen2 .*popup_$1"
+COMMAND="dzen_popup_$1.sh"
 
 if [[ $# -ne 1 ]]; then
     echo -e "Usage:\ndzen_popup_toggle.sh <popup_name>" 1>&2
     exit 1
 fi
-pid=$(pgrep -f "$POPUP")
+pid=$(pgrep -f "$REGEX")
 if [[ -z $pid ]]; then # if popup does not exit
     $COMMAND &
 else
