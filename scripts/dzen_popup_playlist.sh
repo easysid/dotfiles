19021fde7ea23bc748f2bd72eb8639640801ab7d
easysid <easysid+github@gmail.com>
2016-04-27 18:08:29 +0530

    empty playlist fix

--- scripts/dzen_popup_playlist.sh
+++ scripts/dzen_popup_playlist.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 
 # playlist popup for dzen2
-# Thursday, 06 August 2015 13:33 IST
+# Friday, 04 March 2016 14:50 IST
 
-lines=5
+songs=5
 format="%position% [[%artist% - ]%title%]|[%file%]"
 pattern=$(mpc current -f %position%)
-LINES=$(( $lines*2 + 1 ))
+LINES=$(( $songs*2 + 1 ))
 WIDTH=280
-
 source $(dirname $0)/dzen_popup_config
+
+# expand list if there aren't sufficient songs before current one
+[[ $pattern -lt $songs ]] && songs=$(( $songs*2 - $pattern ))
 (
 echo "^fg($titlecol)Playlist^fg()"
-mpc playlist -f "$format" | grep -$lines "$pattern" | while read -r position song; do
+mpc playlist -f "$format" | grep -$songs "\<$pattern\>" | while read -r position song; do
     if [[ $position -eq $pattern ]]; then
         echo "^fg($highlight)$PAD^ca(1, mpc play $position)$song^ca()$PAD"
         continue
