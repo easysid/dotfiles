d81ba204d68bf6541b91995e42c9e42ae8a78333
easysid <easysid+github@gmail.com>
2016-07-01 21:14:24 +0530

    detail_panel.sh replaces toggle conky

---
+++ scripts/detail_panel.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#
+# detail_panel.sh
+# displays a lemonbar segment with more detailed sysinfo
+#
+
+[ $(pgrep -cx detail_panel.sh) -gt 1 ] && killall detail_panel.sh
+
+trap "trap - TERM; kill 0" INT TERM QUIT EXIT
+
+. ~/.scripts/theme_config
+
+# bar geometry
+o=${IFS}
+IFS='x+'
+l=400
+read -r x y gx gy << EOF
+$geometry
+EOF
+IFS=${o}
+geometry="${l}x${y}+$((x+gx-l))+${gy}"
+
+sysinfo -f "%{r}" -l | lemonbar -p \
+        -g "$geometry" \
+        -f "$ICON_FONT" -f "$FONT1"\
+        -B "$BAR_BG" \
+        -F "$BAR_FG" \
+        | sh &
+wait
+
