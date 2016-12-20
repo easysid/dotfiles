9049aedab7d204649781ea3b24badede08e94c94
easysid <easysid+github@gmail.com>
2016-12-20 20:02:08 +0530

    change exit script, and remove Nitrogen keybind

---
+++ scripts/turnoff.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# shutdown dialog. Uses lemonbar
+
+[ $(pgrep -cx turnoff.sh) -gt 1 ] && exit 101
+
+pad='                 '
+
+echo "%{c}%{A:systemctl poweroff:} Shutdown %{A} ${pad} %{A:kill -- -${$}:} Cancel %{A}" |
+        lemonbar -d -p -g 'x200+0+280' -B '#7097544d' -F '#ffdddddd' \
+         -f '-xos4-terminus-bold-r-normal--24-240-72-72-c-120-iso10646-1' \
+         | sh
+
