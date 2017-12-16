076e5586237dc7b96199567d188c0f91b0f5d8a8
easysid <easysid@gmail.com>
2017-12-16 23:10:07 +0530

    add binding for cloning termite in current directory

---
+++ scripts/clone_termite.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# clone_termite.sh - script to open another terminal in the same working dir
+# taken from i3 forums
+# Tuesday, 12 December 2017 21:40 IST
+#
+
+bash_pid=$(xprop -id $(bspc query -N -n) _NET_WM_PID | mawk '{print $3+5}')
+if [ -e "/proc/$bash_pid/cwd" ]; then
+    termite -d "$(readlink /proc/$bash_pid/cwd)" &
+else
+    termite &
+fi
