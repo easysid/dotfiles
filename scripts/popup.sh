b90085239a73ba6d24aff450de840cb29389c56a
easysid <easysid+github@gmail.com>
2018-01-04 21:08:49 +0530

    use lemonbar for with borders for popup

--- scripts/popup.sh
+++ scripts/popup.sh
@@ -1,32 +1,27 @@
 #! /bin/sh
+# a script to pop up whatever is sent to it.
+# taken from z3bra's blog
+# Uses lemonbar fork with borders from github.com/dark-yux/bar
+# Binary is ~/.local/bin/border-lemonbar
+# Thursday, 04 January 2018 17:32 IST
+#
 
-# a script to pop up whatever is sent to it. Uses lemonbar
-# copied from z3bra's blog
-
-. theme_config
+bg='#E52c2c2c'
+fg='#FFf0f0f0'
+bo='#E5B1506A' # border color
+font="sans:size=8"
 
 X=1366
 interval=3
 h=30
-y=20
-pad=4
 
-text="  %{c}${@}   "
-
-# inner bar
-w=$(txtw -f ${FONT1} "${text}" )
+text="${@}"
+w=$(txtw -f "${font}" "$text")
 x=$((X-w-20))
+y=$h
 g="${w}x${h}+${x}+${y}"
 
-# outer bar (for border)
-w0=$((w+2*pad))
-h0=$((h+2*pad))
-x0=$((x-pad))
-y0=$((y-pad))
-g0="${w0}x${h0}+${x0}+${y0}"
-
-# draw outer bar first, followed by inner bar with text
-sleep $interval | lemonbar -d -B '#90f0f0f0' -g "$g0" &
-(echo $text; sleep $interval) | lemonbar -d -B "${BAR_BG}" -F "${BAR_FG}" \
-                                -g "${g}" -f "${FONT1}"
+text="%{c}%{A:exit 0:}$text%{A}"
+(echo $text; sleep $interval) | border-lemonbar -d \
+        -B ${bg} -F ${fg} -R ${bo} -g ${g} -f ${font} -r 3 | sh
 
